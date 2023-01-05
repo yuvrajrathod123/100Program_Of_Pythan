13ac7e35a7764b4e4671ef4f83224278d469bd12
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-05 22:16:08 +0530

    Program to solve the quadratic equation

---
+++ 04_Program4.py
@@ -0,0 +1,13 @@
+
+# Program to solve th quadratic equation
+
+a = float(input("Enter a number a:"))
+b = float(input("Enter a number b:"))
+c = float(input("Enter a number c:"))
+
+d = (b**2)-(4*a*c)
+root1 = (-b+(()**0.5))/2*a
+root2 = (-b-(((b**2)-(4*a*c))**0.5))/2*a
+
+print("root1:",root1)
+print("root2:",root2)
