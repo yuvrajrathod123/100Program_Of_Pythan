5cc55b62964d623072a6512fcd14b04ff6e38092
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-05 09:35:29 +0530

    Area of tiangle

---
+++ 03_program3.py
@@ -0,0 +1,8 @@
+
+# program to print the are if triangle 
+
+base = float(input("Enter the base of tiangle:"))
+height = float(input("Enter the hight of tiangle:"))
+
+area = 0.5 * base * height
+print("Area of the triangle is", area)
