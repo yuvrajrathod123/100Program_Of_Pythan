60e26a35da0f3420c7d531a9461d627c9ff0e7e3
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-05 09:35:03 +0530

    Find the square root

---
+++ 02_Program2.py
@@ -0,0 +1,17 @@
+
+
+
+# Python program to find the square root
+
+
+# Solution1 --> Using Exponentiation2
+
+# num = int(input("Enter a number:"))
+# Sr = num**(1/2)
+# print("Square root of given number is",Sr)
+
+# Solution2 --> Using Math Module
+import math
+num = int(input("Enter a number:"))
+Sr = math.sqrt(num)
+print("The squre root of given number is",Sr)
