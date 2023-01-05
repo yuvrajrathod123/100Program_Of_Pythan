174e6025b1a721cb768f8c144fd7087088475eca
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-05 09:12:26 +0530

    add two number

---
+++ 01_Program1.py
@@ -0,0 +1,15 @@
+# Program to add two numbers
+
+# Solution-1 --> with predefine variable
+
+num1 = 4
+num2 = 7
+sum = num1 + num2
+print("The sum of given two numbers:", sum)
+
+# Solution-2 --> with user input
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter secong number:"))
+sum = num1 + num2
+print("The sum of given two numbers:", sum)
