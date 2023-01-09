ce7193a9e4861ead63c98882b93ca93b34e02589
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-09 21:55:37 +0530

    Program to generate random variable

---
+++ 07_Program7.py
@@ -0,0 +1,9 @@
+
+# Program to generate random variable
+
+import random
+# random.randint(range from where to where)
+# for dice role program we can get the range as 1,6
+num = random.randint(0,10)
+
+print(num)
