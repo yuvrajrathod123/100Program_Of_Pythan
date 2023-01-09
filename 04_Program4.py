bbcae639de0ddee686c3d7a2013c9ad2d68d4c8b
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-09 21:40:46 +0530

    Program to solve the quadratic equation

--- 04_Program4.py
+++ 04_Program4.py
@@ -1,13 +1,21 @@
 
-# Program to solve th quadratic equation
+# Program to solve the quadratic equation (ax**2 + bx + c = 0)
+# a, b, c are real number
+# a != 0
+import cmath
 
-a = float(input("Enter a number a:"))
+a = float(input("Enter a number a (a!=0):"))
 b = float(input("Enter a number b:"))
 c = float(input("Enter a number c:"))
 
+# Formula for Discriminant
 d = (b**2)-(4*a*c)
-root1 = (-b+(()**0.5))/2*a
-root2 = (-b-(((b**2)-(4*a*c))**0.5))/2*a
+root1 = (-b-cmath.sqrt(d))/2*a
+root2 = (-b+cmath.sqrt(d))/2*a
+
 
 print("root1:",root1)
 print("root2:",root2)
+
+print(type(root1))
+print(type(root2))
