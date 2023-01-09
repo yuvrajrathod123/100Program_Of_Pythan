257587701a2162be25ab77175ec275e2794221fe
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-09 22:32:10 +0530

    program to check leaf year

---
+++ 12_Program12.py
@@ -0,0 +1,13 @@
+
+# program to check leaf year
+# year = 365days
+# leaf year = 366days
+
+year = int(input("Enter any year:"))
+
+if (year % 400 == 0) and (year % 100 == 0):
+     print(year,"is a leaf year")
+elif (year % 4 == 0) and (year % 100 != 0):
+    print(year,"is a leaf year")
+else:
+    print(year,"is not the leaf year")
