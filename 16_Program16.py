5f3bafb70ad6ad33f047e0174789e9afc7eee489
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-10 19:22:42 +0530

    program to print the factorial of the number (using recursive function)

---
+++ 16_Program16.py
@@ -0,0 +1,16 @@
+
+# program to print the factorial of the number (using recursive function)
+
+# n = int(input("Enter a number:"))
+# factorial(n)
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+n = int(input("Enter a number:"))
+fact = factorial(n)
+print("factorial of",n,"is",fact)
