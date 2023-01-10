26eff6f38ee5f0b6f65440dede5563363ae53cd2
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-10 17:37:45 +0530

    program to check leap year

--- 12_Program12.py
+++ 12_Program12.py
@@ -1,5 +1,5 @@
 
-# program to check leaf year
+# program to check leap year
 # year = 365days
 # leaf year = 366days
 
@@ -8,6 +8,6 @@ year = int(input("Enter any year:"))
 if (year % 400 == 0) and (year % 100 == 0) or (year % 4 == 0) and (year % 100 != 0):
      print(year,"is a leaf year")
 # elif (year % 4 == 0) and (year % 100 != 0):
-#     print(year,"is a leaf year")
+#     print(year,"is a leap year")
 else:
     print(year,"is not the leaf year")
