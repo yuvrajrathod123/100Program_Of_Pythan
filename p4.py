2feee141377400680952ef87c1ff47fc5f9861cc
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-12 20:42:00 +0530

    number pattern

---
+++ p4.py
@@ -0,0 +1,13 @@
+
+# 1 
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+n = int(input("Enter the number of rows: "))
+ 
+for i in range(1,n):
+    for j in range(1,i+1):
+        print(i, end=" ")
+    print()
