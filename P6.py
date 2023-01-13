850c2f6163370e06f8588c7bbc080587fe3f7093
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-13 20:36:00 +0530

    number pattern

---
+++ P6.py
@@ -0,0 +1,13 @@
+
+# 1 1 1 1 1 
+# 2 2 2 2 2
+# 3 3 3 3 3
+# 4 4 4 4 4
+# 5 5 5 5 5
+
+n = int(input("Enter the no of rows:"))
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i,end=" ")
+    print()
