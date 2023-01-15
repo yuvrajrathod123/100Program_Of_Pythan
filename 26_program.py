97268ef48c50107a118f908e087fe256282d325f
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-15 14:37:16 +0530

    program to remove the elemnt 3 from tje given list  [1,2,3,4,5,6]

---
+++ 26_program.py
@@ -0,0 +1,12 @@
+
+
+# program to remove the elemnt 3 from tje given list  [1,2,3,4,5,6]
+
+list = [1,2,3,4,5,6]
+
+print("current list:",list)
+
+list.remove(3)
+print("removed element:",n)
+
+print("updated list",list)
