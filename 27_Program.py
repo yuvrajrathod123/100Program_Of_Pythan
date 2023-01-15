4c6b574c08f19e989f573bb0d045153272f43263
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-15 14:41:55 +0530

    program to insert the element "python" on index 2 in the list

---
+++ 27_Program.py
@@ -0,0 +1,10 @@
+
+# program to insert the element "python" on index 2 in the list
+
+list = ['java','php','c','c++']
+
+print("Current list",list)
+
+list.insert(2,'python')
+
+print("updated list",list)
