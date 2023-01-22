7940b0b59df596acd4b210b4b985c10fabeb0f13
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-22 23:05:13 +0530

    program to find ASCII value of charecter

---
+++ 35_Program.py
@@ -0,0 +1,7 @@
+
+# program to find ASCII value of charecter
+
+char = input("Enter the chrecter from A to Z or from a to z or any special charecter:")
+
+print("The ASCII value for",char,"is:",ord(char))
+
