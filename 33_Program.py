fde191982202f1a68a93419ecc26314b61922ca4
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-22 22:51:41 +0530

    program to find number divisible by another number using for loop

--- 33_Program.py
+++ 33_Program.py
@@ -1,10 +1,19 @@
 
 # program to find number divisible by another number using for loop
 
-n = int(input("Enter a number: "))
-print("the numbers divisible by",n,"in range 1 to 100")
+# n = int(input("Enter a number: "))
+# print("the numbers divisible by",n,"in range 1 to 100")
 
-for i in range(1,100):
-    if i % n == 0:
-        print(i)
+# for i in range(1,100):
+#     if i % n == 0:
+#         print(i)
+
+
+# program to find number divisible by another number using list
+
+l = [26,39,33,45,65,91,78,45]
+
+result = list(filter(lambda x : x % 13 == 0,l))
+
+print("The number divisible by 13 are:",result)
 
