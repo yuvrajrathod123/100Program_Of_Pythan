6282642560341430cd66f4c0e1630ec984100db1
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-03-01 01:00:05 +0530

    program to illustrate different set operation

---
+++ 53_program.py
@@ -0,0 +1,10 @@
+
+# program to illustrate different set operation
+
+A = {1,2,3,6,8,9}
+B = {3,4,5,6,7,8}
+
+print("The union is:", A | B)
+print("The intersection is:", A & B)
+print("The difference is:", A - B)
+print("The Symmetric diffrerence is:", A ^ B)
